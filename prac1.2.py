fffe763d95624300709c42f0721b4e07c7a00188
AnshuPatel <patelanshu0027@gmail.com>
2024-11-07 11:03:19 +0530

    pandas and matplot

---
+++ prac1.2.py
@@ -0,0 +1,27 @@
+# Pandas
+import pandas as pd
+print("IU2141230160")
+data = {
+'Name': ['AP', 'Kismat', 'Ansh'], 'Age': [21, 22, 30],
+'City': ['New York', 'San Francisco', 'Los Angeles']
+}
+df = pd.DataFrame(data)
+print("DataFrame:")
+print(df)
+ages = df['Age']
+print("\nAges:")
+print(ages)
+filtered_df = df[df['Age'] > 28]
+print("\nFiltered DataFrame (Age > 28):")
+print(filtered_df)
+
+# matplotlib
+import matplotlib.pyplot as plt
+x = [1, 3, 5, 7, 9]
+y = [2, 4, 5, 7, 11]
+plt.plot(x, y, marker='o')
+plt.title("Sample Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.grid(True)
+plt.show()
